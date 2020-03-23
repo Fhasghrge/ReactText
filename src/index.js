d18151d0d4849334114c7ffff9c7c6f20e8450f7
Fhasghrge <2490300986@qq.com>
2020-03-23 21:35:03 +0800

    day01

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Tab extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            c1: 'content active',
+            c2: 'content'
+        }
+        this.clickEvent = this.clickEvent.bind(this)
+    }
+    clickEvent(e) {
+        let index = e.target.dataset.index
+        if (index === '1') {
+            this.setState({
+                c1: 'content active'
+            })
+            this.setState({
+                c2: "content"
+            })
+        } else {
+            this.setState({
+                c1: 'content',
+                c2: "content active"
+            })
+        }
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render() {
+        return (
+            <div>
+                <button data-index='1' onClick={this.clickEvent}>content 1</button>
+                <button data-index='2' onClick={this.clickEvent}>content 2</button>
+                <div className={this.state.c1}>
+                    <h1>main 1</h1>
+                </div>
+                <div className={this.state.c2}>
+                    <h1>main 2</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDom.render(
+    <Tab />,
+    document.querySelector('#root')
+)
