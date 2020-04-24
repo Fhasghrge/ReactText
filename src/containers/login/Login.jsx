cef07f6ea5adfbd6d9669efb7649e03809b5e87c
Fhasghrge <2490300986@qq.com>
2020-04-25 00:22:29 +0800

    use antd do login page

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import './login.css';
-
+// import './login.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import './login.css'
 export default class Login extends React.Component {
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
@@ -9,30 +11,73 @@ export default class Login extends React.Component {
       loginInfo: {
         name: '',
         passwd: '',
-      }
+      },
     };
   }
-  login = () => {};
-  signIn = () => {};
+  onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
   render() {
     return (
       <div id="login">
-        <div id="login-main">
-          <div className="inputs">
-            <div className="inpt">
-              <label htmlFor="username">用户名</label>
-              <input type="text" name="用户名" id="username" />
-            </div>
-            <div className="inpt">
-              <label htmlFor="passwd">密码</label>
-              <input type="password" name="passwd" id="passwd" />
-            </div>
-          </div>
-          <div id="btns">
-            <button onClick={this.login}>登录</button>
-            <button onClick={this.signIn}>注册</button>
-          </div>
-        </div>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={this.onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="zhang">
+              Forgot password
+            </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+            Or <a href="zhang">register now!</a>
+          </Form.Item>
+        </Form>
       </div>
     );
   }
